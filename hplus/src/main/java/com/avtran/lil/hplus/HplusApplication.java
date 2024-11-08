98c3f2fdd658b3f7b6118a24fad210ca5344299c
Andy Tran <undahvt@gmail.com>
2024-11-07 21:13:53 -0800

    spring initalizr

---
+++ hplus/src/main/java/com/avtran/lil/hplus/HplusApplication.java
@@ -0,0 +1,13 @@
+package com.avtran.lil.hplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HplusApplication.class, args);
+	}
+
+}
